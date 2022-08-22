b721f62784aca87eedd021045c3042a8d1101ef5
Rana2020Sameh <105318792+Rana2020Sameh@users.noreply.github.com>
2022-08-22 21:02:19 +0200

    Update InvListin.java

--- src/com/sig/actions/InvListin.java
+++ src/com/sig/actions/InvListin.java
@@ -1,11 +1,13 @@
-package com.sig.actions;
-import com.sig.format.InvCol;
-import com.sig.format.InvColTbMod;
-import com.sig.format.InvRows;
-import com.sig.format.InvRowsTbMod;
-import com.sig.design.InvDesign;
-import com.sig.design.InvColDil;
-import com.sig.design.InvRowDil;
+
+package com.sig.controller;
+
+import com.sig.model.InvoiceHeader;
+import com.sig.model.InvoiceHeaderTableModel;
+import com.sig.model.InvoiceLine;
+import com.sig.model.InvoiceLineTableModel;
+import com.sig.view.InvoiceFrame;
+import com.sig.view.InvoiceHeaderDialog;
+import com.sig.view.InvoiceLineDialog;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -21,41 +23,42 @@ import java.util.Date;
 import java.util.List;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-public class InvListin implements ActionListener {
 
-    private InvDesign Design;
-    private InvColDil ColDil;
-    private InvRowDil RowDil;
+public class InvoiceActionListener implements ActionListener {
 
-    public InvListin(InvDesign Design) {
-        this.Design = Design;
+    private InvoiceFrame frame;
+    private InvoiceHeaderDialog headerDialog;
+    private InvoiceLineDialog lineDialog;
+
+    public InvoiceActionListener(InvoiceFrame frame) {
+        this.frame = frame;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
-            case "Saveing":
-                Saveing();
+            case "Save Files":
+                saveFiles();
                 break;
 
-            case "Import":
-                Import();
+            case "Load Files":
+                loadFiles();
                 break;
 
-            case "genrateInv":
-                genrateInv();
+            case "New Invoice":
+                createNewInvoice();
                 break;
 
-            case "CleareInv":
-                CleareInv();
+            case "Delete Invoice":
+                deleteInvoice();
                 break;
 
-            case "CreatRow":
-                CreatRow();
+            case "New Line":
+                createNewLine();
                 break;
 
-            case "ClearRow":
-                ClearRow();
+            case "Delete Line":
+                deleteLine();
                 break;
 
             case "newInvoiceOK":
@@ -76,120 +79,120 @@ public class InvListin implements ActionListener {
         }
     }
 
-    private void Import() {
-        JFileChooser selectFile = new JFileChooser();
+    private void loadFiles() {
+        JFileChooser fileChooser = new JFileChooser();
         try {
-            int Outtpoot = selectFile.showOpenDialog(Design);
-            if (Outtpoot == JFileChooser.APPROVE_OPTION) {
-                File colFile = selectFile.getSelectedFile();
-                Path colPath = Paths.get(colFile.getAbsolutePath());
-                List<String> colLines = Files.readAllLines(colPath);
-                ArrayList<InvCol> invoicecolumns = new ArrayList<>();
-                for (String ColumnsLine : colLines) {
-                    String[] atrb = ColumnsLine.split(",");
-                    String select1 = atrb[0];
-                    String select2 = atrb[1];
-                    String select3 = atrb[2];
-                    int Lebel = Integer.parseInt(select1);
-                    Date invoiceDate = InvDesign.dF.parse(select2);
-                    InvCol Coll = new InvCol(Lebel, select3, invoiceDate);
-                    invoicecolumns.add(Coll);
+            int result = fileChooser.showOpenDialog(frame);
+            if (result == JFileChooser.APPROVE_OPTION) {
+                File headerFile = fileChooser.getSelectedFile();
+                Path headerPath = Paths.get(headerFile.getAbsolutePath());
+                List<String> headerLines = Files.readAllLines(headerPath);
+                ArrayList<InvoiceHeader> invoiceHeaders = new ArrayList<>();
+                for (String headerLine : headerLines) {
+                    String[] arr = headerLine.split(",");
+                    String str1 = arr[0];
+                    String str2 = arr[1];
+                    String str3 = arr[2];
+                    int code = Integer.parseInt(str1);
+                    Date invoiceDate = InvoiceFrame.dateFormat.parse(str2);
+                    InvoiceHeader header = new InvoiceHeader(code, str3, invoiceDate);
+                    invoiceHeaders.add(header);
                 }
-                Design.setInvoicesArray(invoicecolumns);
+                frame.setInvoicesArray(invoiceHeaders);
 
-                Outtpoot = selectFile.showOpenDialog(Design);
-                if (Outtpoot == JFileChooser.APPROVE_OPTION) {
-                    File RowFile = selectFile.getSelectedFile();
-                    Path rowPath = Paths.get(RowFile.getAbsolutePath());
-                    List<String> lineLines = Files.readAllLines(rowPath);
-                    ArrayList<InvRows> invoiceLines = new ArrayList<>();
+                result = fileChooser.showOpenDialog(frame);
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    File lineFile = fileChooser.getSelectedFile();
+                    Path linePath = Paths.get(lineFile.getAbsolutePath());
+                    List<String> lineLines = Files.readAllLines(linePath);
+                    ArrayList<InvoiceLine> invoiceLines = new ArrayList<>();
                     for (String lineLine : lineLines) {
-                        String[] atrb = lineLine.split(",");
-                        String select1 = atrb[0];    
-                        String select2 = atrb[1];    
-                        String select3 = atrb[2];   
-                        String select4 = atrb[3];   
-                        int invoiceleble = Integer.parseInt(select1);
-                        double Fee = Double.parseDouble(select3);
-                        int Number = Integer.parseInt(select4);
-                        InvCol invoice = Design.getInvObject(invoiceleble);
-                        InvRows ls = new InvRows(select2, Fee, Number, invoice);
-                        invoice.getLs().add(ls);
+                        String[] arr = lineLine.split(",");
+                        String str1 = arr[0];    
+                        String str2 = arr[1];    
+                        String str3 = arr[2];    
+                        String str4 = arr[3];    
+                        int invCode = Integer.parseInt(str1);
+                        double price = Double.parseDouble(str3);
+                        int count = Integer.parseInt(str4);
+                        InvoiceHeader inv = frame.getInvObject(invCode);
+                        InvoiceLine line = new InvoiceLine(str2, price, count, inv);
+                        inv.getLines().add(line);
                     }
                 }
-                InvColTbMod colTbMo = new InvColTbMod(invoicecolumns);
-                Design.setColTbMod(colTbMo);
-                Design.getInvHTbl().setModel(colTbMo);
+                InvoiceHeaderTableModel headerTableModel = new InvoiceHeaderTableModel(invoiceHeaders);
+                frame.setHeaderTableModel(headerTableModel);
+                frame.getInvHTbl().setModel(headerTableModel);
                 System.out.println("files read");
             }
 
         } catch (IOException ex) {
-            JOptionPane.showMessageDialog(Design, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(frame, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         } catch (ParseException ex) {
-            JOptionPane.showMessageDialog(Design, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(frame, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    private void genrateInv() {
-        ColDil = new InvColDil(Design);
-        ColDil.setVisible(true);
+    private void createNewInvoice() {
+        headerDialog = new InvoiceHeaderDialog(frame);
+        headerDialog.setVisible(true);
     }
 
-    private void CleareInv() {
-        int selectedInvoiceIndex = Design.getInvHTbl().getSelectedRow();
+    private void deleteInvoice() {
+        int selectedInvoiceIndex = frame.getInvHTbl().getSelectedRow();
         if (selectedInvoiceIndex != -1) {
-            Design.getInvoicesArray().remove(selectedInvoiceIndex);
-            Design.getColTbMod().fireTableDataChanged();
+            frame.getInvoicesArray().remove(selectedInvoiceIndex);
+            frame.getHeaderTableModel().fireTableDataChanged();
 
-            Design.getInvLTbl().setModel(new InvRowsTbMod(null));
-            Design.setLinesArray(null);
-            Design.getCustNameLbl().setText("");
-            Design.getInvNumLbl().setText("");
-            Design.getInvTotalIbl().setText("");
-            Design.getInvDateLbl().setText("");
+            frame.getInvLTbl().setModel(new InvoiceLineTableModel(null));
+            frame.setLinesArray(null);
+            frame.getCustNameLbl().setText("");
+            frame.getInvNumLbl().setText("");
+            frame.getInvTotalIbl().setText("");
+            frame.getInvDateLbl().setText("");
         }
     }
 
-    private void CreatRow() {
-        RowDil = new InvRowDil(Design);
-        RowDil.setVisible(true);
+    private void createNewLine() {
+        lineDialog = new InvoiceLineDialog(frame);
+        lineDialog.setVisible(true);
     }
 
-    private void ClearRow() {
-        int selectedLineIndex = Design.getInvLTbl().getSelectedRow();
-        int selectedInvoiceIndex = Design.getInvHTbl().getSelectedRow();
+    private void deleteLine() {
+        int selectedLineIndex = frame.getInvLTbl().getSelectedRow();
+        int selectedInvoiceIndex = frame.getInvHTbl().getSelectedRow();
         if (selectedLineIndex != -1) {
-            Design.getLinesArray().remove(selectedLineIndex);
-            InvRowsTbMod lineTableModel = (InvRowsTbMod) Design.getInvLTbl().getModel();
+            frame.getLinesArray().remove(selectedLineIndex);
+            InvoiceLineTableModel lineTableModel = (InvoiceLineTableModel) frame.getInvLTbl().getModel();
             lineTableModel.fireTableDataChanged();
-            Design.getInvTotalIbl().setText("" + Design.getInvoicesArray().get(selectedInvoiceIndex).getInvCount());
-            Design.getColTbMod().fireTableDataChanged();
-            Design.getInvHTbl().setRowSelectionInterval(selectedInvoiceIndex, selectedInvoiceIndex);
+            frame.getInvTotalIbl().setText("" + frame.getInvoicesArray().get(selectedInvoiceIndex).getInvoiceTotal());
+            frame.getHeaderTableModel().fireTableDataChanged();
+            frame.getInvHTbl().setRowSelectionInterval(selectedInvoiceIndex, selectedInvoiceIndex);
         }
     }
 
-    private void Saveing() {
-        ArrayList<InvCol> invoicesArray = Design.getInvoicesArray();
+    private void saveFiles() {
+        ArrayList<InvoiceHeader> invoicesArray = frame.getInvoicesArray();
         JFileChooser fc = new JFileChooser();
         try {
-            int result = fc.showSaveDialog(Design);
+            int result = fc.showSaveDialog(frame);
             if (result == JFileChooser.APPROVE_OPTION) {
                 File headerFile = fc.getSelectedFile();
                 FileWriter hfw = new FileWriter(headerFile);
                 String headers = "";
                 String lines = "";
-                for (InvCol invoice : invoicesArray) {
+                for (InvoiceHeader invoice : invoicesArray) {
                     headers += invoice.toString();
                     headers += "\n";
-                    for (InvRows line : invoice.getLs()) {
+                    for (InvoiceLine line : invoice.getLines()) {
                         lines += line.toString();
                         lines += "\n";
                     }
                 }
-           
+            
                 headers = headers.substring(0, headers.length()-1);
                 lines = lines.substring(0, lines.length()-1);
-                result = fc.showSaveDialog(Design);
+                result = fc.showSaveDialog(frame);
                 File lineFile = fc.getSelectedFile();
                 FileWriter lfw = new FileWriter(lineFile);
                 hfw.write(headers);
@@ -198,80 +201,80 @@ public class InvListin implements ActionListener {
                 lfw.close();
             }
         } catch (IOException ex) {
-            JOptionPane.showMessageDialog(Design, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(frame, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
     private void newInvoiceDialogCancel() {
-        ColDil.setVisible(false);
-        ColDil.dispose();
-        ColDil = null;
+        headerDialog.setVisible(false);
+        headerDialog.dispose();
+        headerDialog = null;
     }
 
     private void newInvoiceDialogOK() {
-        ColDil.setVisible(false);
+        headerDialog.setVisible(false);
 
-        String custName = ColDil.getCNm().getText();
-        String str = ColDil.getInvoiceD().getText();
+        String custName = headerDialog.getCustNameField().getText();
+        String str = headerDialog.getInvDateField().getText();
         Date d = new Date();
         try {
-            d = InvDesign.dF.parse(str);
+            d = InvoiceFrame.dateFormat.parse(str);
         } catch (ParseException ex) {
-            JOptionPane.showMessageDialog(Design, "Cannot parse date, resetting to today.", "Invalid date format", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(frame, "Cannot parse date, resetting to today.", "Invalid date format", JOptionPane.ERROR_MESSAGE);
         }
 
         int invNum = 0;
-        for (InvCol inv : Design.getInvoicesArray()) {
-            if (inv.getNm() > invNum) {
-                invNum = inv.getNm();
+        for (InvoiceHeader inv : frame.getInvoicesArray()) {
+            if (inv.getNum() > invNum) {
+                invNum = inv.getNum();
             }
         }
         invNum++;
-        InvCol newInv = new InvCol(invNum, custName, d);
-        Design.getInvoicesArray().add(newInv);
-        Design.getColTbMod().fireTableDataChanged();
-        ColDil.dispose();
-        ColDil = null;
+        InvoiceHeader newInv = new InvoiceHeader(invNum, custName, d);
+        frame.getInvoicesArray().add(newInv);
+        frame.getHeaderTableModel().fireTableDataChanged();
+        headerDialog.dispose();
+        headerDialog = null;
     }
 
     private void newLineDialogCancel() {
-        RowDil.setVisible(false);
-        RowDil.dispose();
-        RowDil = null;
+        lineDialog.setVisible(false);
+        lineDialog.dispose();
+        lineDialog = null;
     }
 
     private void newLineDialogOK() {
-        RowDil.setVisible(false);
+        lineDialog.setVisible(false);
 
-        String name = RowDil.getPiceNm().getText();
-        String select1 = RowDil.getpiceQuantity().getText();
-        String select2 = RowDil.getPicePr().getText();
+        String name = lineDialog.getItemNameField().getText();
+        String str1 = lineDialog.getItemCountField().getText();
+        String str2 = lineDialog.getItemPriceField().getText();
         int count = 1;
         double price = 1;
         try {
-            count = Integer.parseInt(select1);
+            count = Integer.parseInt(str1);
         } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(Design, "Unable To conver number", "Wrong Number Design", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(frame, "Cannot convert number", "Invalid number format", JOptionPane.ERROR_MESSAGE);
         }
 
         try {
-            price = Double.parseDouble(select2);
+            price = Double.parseDouble(str2);
         } catch (NumberFormatException ex) {
-            JOptionPane.showMessageDialog(Design, "Unable To conver price", "Wrong Number Design", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(frame, "Cannot convert price", "Invalid number format", JOptionPane.ERROR_MESSAGE);
         }
-        int selectedInvHeader = Design.getInvHTbl().getSelectedRow();
+        int selectedInvHeader = frame.getInvHTbl().getSelectedRow();
         if (selectedInvHeader != -1) {
-            InvCol invHeader = Design.getInvoicesArray().get(selectedInvHeader);
-            InvRows line = new InvRows(name, price, count, invHeader);
+            InvoiceHeader invHeader = frame.getInvoicesArray().get(selectedInvHeader);
+            InvoiceLine line = new InvoiceLine(name, price, count, invHeader);
             //invHeader.getLines().add(line);
-            Design.getLinesArray().add(line);
-            InvRowsTbMod lineTableModel = (InvRowsTbMod) Design.getInvLTbl().getModel();
+            frame.getLinesArray().add(line);
+            InvoiceLineTableModel lineTableModel = (InvoiceLineTableModel) frame.getInvLTbl().getModel();
             lineTableModel.fireTableDataChanged();
-            Design.getColTbMod().fireTableDataChanged();
+            frame.getHeaderTableModel().fireTableDataChanged();
         }
-        Design.getInvHTbl().setRowSelectionInterval(selectedInvHeader, selectedInvHeader);
-        RowDil.dispose();
-        RowDil = null;
+        frame.getInvHTbl().setRowSelectionInterval(selectedInvHeader, selectedInvHeader);
+        lineDialog.dispose();
+        lineDialog = null;
     }
 
 }
